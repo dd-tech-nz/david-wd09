53c9a80d4ec47c4147030a7a2a53b688b61553f2
dd-tech-nz <ddumolo@hotmail.com>
2020-04-11 20:39:46 +1200

    refactor components, add fade in functionality

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Projects from './Projects'
+import SocialProfiles from './SocialProfiles'
+import profile from '../assets/profile.png'
+import Title from './Title'
+
+
+class App extends Component {
+  state = { displayBio: false };
+  // constructor() {
+  //   super()
+  //   this.state = { displayBio: false }
+
+  //   console.log('Component this', this)
+
+  //   this.toggleDisplayBio = this.toggleDisplayBio.bind(this)
+ 
+  // }
+
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio })
+   
+  }
+  
+  render() {
+    let bio = this.state.displayBio ?  (
+      <div>
+          <p>I live in Auckland, and code every day.</p>
+          <p>My favourite langauge is JavaScript, and I think React.js is awesome.</p>
+        <p>Besides coding, I also love martial arts and my dog!</p>
+        <button onClick={this.toggleDisplayBio}>Show less</button>
+      </div>
+    ) : (
+        <div>
+          <button onClick={this.toggleDisplayBio}>Read more</button>
+        </div>
+    )
+
+    return (
+      <div>
+        <img src={profile} alt='profile' className="profile"/>
+        <h1>Hello</h1>
+        <p>My name is David.</p>
+        <Title />
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        {bio}
+
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+      </div>
+      
+      
+    )
+  }
+}
+
+export default App;
